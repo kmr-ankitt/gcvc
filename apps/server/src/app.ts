d6a368f9577a641ce6e73e432fbb07933f796ea2
ankit <sahuankit.murhu@gmail.com>
2025-06-06 21:54:50 +0530

    feat(server): raw structure of server

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import { PORT } from "./config";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 export const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("New user connected.")
+})
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })

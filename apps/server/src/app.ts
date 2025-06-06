38065aa9f7745864a4270887e595efa213cc4de6
ankit <sahuankit.murhu@gmail.com>
2025-06-06 19:45:19 +0530

    fix: revert back

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,33 +1,8 @@
 import express from "express";
 import { PORT } from "./config";
-import http from "http";
-import { Server } from "socket.io";
 
 export const app = express();
-const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  }
-});
-
-io.on("connection", (socket) => {
-  console.log("User connected with id:", socket.id);
-
-  socket.on('offer', (payload) => {
-    socket.broadcast.emit('offer', payload);
-  });
-
-  socket.on('answer', (payload) => {
-    socket.broadcast.emit('answer', payload);
-  });
-
-  socket.on('ice-candidate', (payload) => {
-    socket.broadcast.emit('ice-candidate', payload);
-  });
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,10 +1,8 @@
-import VideoSection from "../components/VideoSection";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <VideoSection />
     </div>
   );
 }

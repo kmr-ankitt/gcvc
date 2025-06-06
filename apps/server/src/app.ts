bc7ca3fe6f9cf2f045eea29fff94fed1a790139d
ankit <sahuankit.murhu@gmail.com>
2025-06-07 00:22:21 +0530

    feat(server): socket impl on backend

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,17 +2,25 @@ import express from "express";
 import { PORT } from "./config";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import { UserManager } from "./manager/UserManager";
 
 export const app = express();
 const server = createServer(app);
-const io = new Server({
+const io = new Server(server, {
   cors: {
     origin: "*"
   }
 });
 
+const userManager = new UserManager();
+
 io.on("connection", (socket) => {
   console.log("New user connected.")
+  userManager.addUser("ankit", socket);
+  io.on("disconnect", () => {
+    userManager.removeUser(socket);
+    console.log("User disconnected.")
+  })
 })
 
 server.listen(PORT, () => {

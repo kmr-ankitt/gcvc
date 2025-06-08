e58376c4904eee915275a6f5fae0e462f56c8ceb
ankit <sahuankit.murhu@gmail.com>
2025-06-08 16:19:38 +0530

    fix(client): setting favicon

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "GCVC - Video Chat",
   description: "GCVC - Video Chat",
+  icons: {
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({
